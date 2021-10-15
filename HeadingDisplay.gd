7e64938b226083dbfe36298734bcd6ef5edf2e75
Suchaaver Chahal <suchaaverchahal@gmail.com>
2021-10-15 10:42:09 -0700

    letters are coming together decently

--- HeadingDisplay.gd
+++ HeadingDisplay.gd
@@ -2,7 +2,15 @@ extends Spatial
 
 onready var tween = $Pivot/Tween
 
-	
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	if tween.is_active():
+		$Pivot/w_key.look_at(Vector3(-100, -100, -100), Vector3.UP)
+		$Pivot/s_key.look_at(Vector3(-100,-100,-100), Vector3.UP)
+		$Pivot/d_key.look_at(Vector3(-100,-100,-100), Vector3.UP)
+		
 func _rotate_heading(positive) -> void:
 	print("SIGNALED")
 	if (not positive):
@@ -10,4 +18,5 @@ func _rotate_heading(positive) -> void:
 	else:
 		tween.interpolate_property($Pivot, "rotation:y", $Pivot.rotation.y, $Pivot.rotation.y + deg2rad(-90.0), 0.2)
 	tween.start()
+
 
