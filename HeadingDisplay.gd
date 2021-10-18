54a26536552a6a6767a2b8fa1168a58b47f6c626
Suchaaver Chahal <suchaaverchahal@gmail.com>
2021-10-17 17:55:41 -0700

    shaders

--- HeadingDisplay.gd
+++ HeadingDisplay.gd
@@ -16,11 +16,10 @@ func _process(delta: float) -> void:
 		$Pivot/d_key.look_at(Vector3(-100,-100,-100), Vector3.UP)
 		
 func _rotate_heading(positive) -> void:
-	print("SIGNALED")
 	if (not positive):
-		tween.interpolate_property($Pivot, "rotation:y", $Pivot.rotation.y, $Pivot.rotation.y + deg2rad(90.0), 0.2)
+		tween.interpolate_property($Pivot, "rotation:y", $Pivot.rotation.y, $Pivot.rotation.y + deg2rad(90.0), 0.15)
 	else:
-		tween.interpolate_property($Pivot, "rotation:y", $Pivot.rotation.y, $Pivot.rotation.y + deg2rad(-90.0), 0.2)
+		tween.interpolate_property($Pivot, "rotation:y", $Pivot.rotation.y, $Pivot.rotation.y + deg2rad(-90.0), 0.15)
 	tween.start()
 
 
