b60649deacb443cb4df69de76cb71c62dfdadcc4
Suchaaver Chahal <suchaaverchahal@gmail.com>
2021-10-21 20:27:49 -0700

    tween don't work but i sure as hell try

--- Cube.gd
+++ Cube.gd
@@ -1,7 +1,15 @@
 extends Spatial
 class_name Cube
 
+var shrink_speed = 5
+
 export(Color, RGBA) var base_color
 
 func _ready() -> void:
+	$AnimationPlayer.connect("animation_finished", self, "queue_free")
+	
+func _physics_process(delta: float) -> void:
 	pass
+#	scale = scale.linear_interpolate(Vector3(0,0,0), delta * shrink_speed)
+func eliminate():
+	queue_free()
